54b9294ccaa88cc04915468c145117eb6de817a0
glebsonIfba <glebsonifba@gmail.com>
2025-02-25 20:41:31 -0300

    criação das classe cliente,serviço e ordem de serviço

---
+++ src/main/OrdenDeServiço.java
@@ -0,0 +1,9 @@
+public class OrdemDeServico{
+    private Int idOs;
+    private String descricao;
+    private Date dataAbertura;
+    private Date dataFechamento;
+    private String status;
+
+    
+}
---
+++ src/main/Servico.java
@@ -0,0 +1,8 @@
+public class Servico{
+    private Int idServico;
+    private String tipo;
+    private String descricao;
+    private Float valor;
+    
+
+}
